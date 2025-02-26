c68455eb9673489ccdfe9dbe250441a05b8e9a12
alessia-mol <alessia.molignini@outlook.de>
2025-02-26 10:26:47 +0100

    autor entfernt

--- BMLD-Inf2-AppV0-main/Start.py
+++ BMLD-Inf2-AppV0-main/Start.py
@@ -13,5 +13,4 @@ Diese App wurde von folgenden Personen entwickelt:
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
-Autor: Samuel Wehrli (wehs@zhaw.ch)
 """
